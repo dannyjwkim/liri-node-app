a02e327f22fd869e0eb9ca7e72ea9abdcb81cff3
Daniel Kim <dannyjwkim@Daniels-MacBook-Pro.local>
2017-04-07 21:02:38 -0400

    main switch statement

--- liri.js
+++ liri.js
@@ -1,14 +1,35 @@
-var inputCommand = process.argv[2];
-var title = process.argv[3];
-console.log(inputCommand);
-console.log(title);
-
+var fs = require("fs");
 var keys = require("./keys.js")
 var request = require("request");
 var spotify = require("spotify");
 var twitter = require("twitter");
 
+var inputCommand = process.argv[2];
+// var title = process.argv[3];
+// console.log(inputCommand);
+// console.log(title);
 
+switch(inputCommand) {
+	case "my-tweets": 
+		myTweets(); 
+		break;
+	case "spotify-this-song": 
+		spotifyThisSong(); 
+		break;
+	case "movie-this": 
+		movieThis(); 
+		break;
+	case "do-what-it-says": 
+		doWhatItSays(); 
+		break;
+	// Instructions displayed in terminal to the user
+	default: console.log("\r\n" +"Try typing one of the following commands after 'node liri.js' : " +"\r\n"+
+		"1. my-tweets 'any twitter name' " +"\r\n"+
+		"2. spotify-this-song 'any song name' "+"\r\n"+
+		"3. movie-this 'any movie name' "+"\r\n"+
+		"4. do-what-it-says."+"\r\n"+
+		"Be sure to put the movie or song name in quotation marks if it's more than one word.");
+	};
 
 var client = new twitter({
 consumer_key: keys.twitterKeys.consumer_key,
